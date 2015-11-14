166d5c2356cc171cf61da64050773097575794cd
sachalmalick <sachalmalick@gmail.com>
2015-11-14 15:35:08 -0500

    Sachal.java changes
    made some changes to the sachal

--- Sachal.java
+++ Sachal.java
@@ -13,31 +13,62 @@ public class Sachal extends Character{
     public Sachal(String newName){
 	// Sets attributes
 	super(newName);
+	hp = 75;
+	strength = 60;
+	defense = 30;
+	attack = 0.3;
+	
     }
 	
-    // public  boolean isAlive(){
-    // 	return hp > 0; // If the "hp" is above 0, return true;
-    // }
+    public void lowerHP(int dmg){
+	double x = Math.random();
+	if (x < 1.0f/3.0f)
+	    hp = hp; // returns the "hp" after the "damage" has been applied
+	else
+	    hp -= dmg;
+    }
 
-    // public int getDefense(){
-    // 	return defense; // returns the defense
-    // }
+    public int attack(Monster mon){
+    	
+    	return tryToBeMean(mon);
 
-    // public String getName(){
-    // 	return name; // returns the name
-    // }
-
-    // public void lowerHP(int dmg){
-    // 	hp -= dmg; // returns the "hp" after the "damage" has been applied
-    // }
-
-    // public int attack(Monster mon){
-    // 	int damage;
-    // 	damage = (int)(strength * attack) - mon.getDefense(); // sets damage
-    // 	mon.lowerHP(damage);
-    // 	return damage;
-    // }
+    }
+    
+    
+    public int tryToBeMean(Monster mon) {
+		int s = (int) (Math.random() * 2);
+		if (s == 1) {
+			return confuse(mon);
+		}
+		else {
+			
+			return accidentallRaiseMonstersSelfAstee(mon);
+		}
+    }
+    
+    public int accidentallRaiseMonstersSelfAstee(Monster mon) {
+    	return 0;
+    }
+    
+    public int confuse (Monster mon) {
+    	int damage;
+    	damage = (int)(strength * attack) - mon.getDefense(); // sets damage
+        mon.lowerHP(damage);
+    	return damage;
+    }
 	
+    public void normalize() {
+		
+    	defense = 25;
+    	attack = .3;
+    		
+        }
+    	
+        public void specialize() {
+    	defense-=10;
+    	attack+=1;
+        }
+
 
 
 }
